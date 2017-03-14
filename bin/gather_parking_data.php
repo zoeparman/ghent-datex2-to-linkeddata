3b2b7851fd4e25f63f86f9a5d4d1b282e17e58fa
Arne Gevaert <arne.gevaert@ugent.be>
2017-03-14 15:40:26 +0100

    Fragment Manager, deploying wrapped fragments, change into cron job

--- bin/gather_parking_data.php
+++ bin/gather_parking_data.php
@@ -2,16 +2,17 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // TODO add command line arguments (amount of queries, location to save file)
+// TODO use caching headers to determine if querying is necessary
 
-use \otn\linkeddatex2\gather\GraphProcessor;
-use \otn\linkeddatex2\gather\FragmentedWriter;
+use otn\linkeddatex2\gather\GraphProcessor;
+use otn\linkeddatex2\gather\FragmentManager;
 
 $start_time = time();
 $duration = 60*60*4; // 4 hours of recording
 $KiB = 1024; // readability
 
 date_default_timezone_set("Europe/Brussels");
-$writer = new FragmentedWriter(date("Ymd"), __DIR__ . '/out', 10*$KiB); // 10 KiB for testing
+$writer = new FragmentManager(date("Ymd"), __DIR__ . '/out', 10*$KiB); // 10 KiB for testing
 $writer->clear_directory(); // Start over from 0 (for testing)
 $interval = 30; // TODO use caching headers when available
 $static_data_written = false;
@@ -23,12 +24,12 @@ while (time() - $start_time < $duration) {
 
     if (!$static_data_written) {
         $static_data = GraphProcessor::strip_static_data_from_parkings($parkings);
-        $writer->write(serialize($static_data)); // TODO PHP serialize or JSON?
+        $writer->write(json_encode($static_data));
         $static_data_written = true;
     }
 
     $dynamic_data = GraphProcessor::strip_dynamic_data_from_parkings($parkings);
-    $writer->write(serialize($dynamic_data));
+    $writer->write(json_encode($dynamic_data));
     print($dynamic_data["time"] . "\n");
     sleep($interval);
 }
--- src/otn/linkeddatex2/gather/FragmentManager.php
+++ src/otn/linkeddatex2/gather/FragmentManager.php
@@ -11,9 +11,10 @@ use \League\Flysystem\Filesystem;
  * A file writer using FlySystem that splits a stream of data into equally sized chunks
  * using a [basename]-[chunk_number] format.
  */
-class FragmentedWriter
+class FragmentManager
 {
     private $current_fragment;
+    private $wrapped_fragments;
     private $base_filename;
     private $filesystem;
     private $max_file_size;
@@ -32,9 +33,19 @@ class FragmentedWriter
         $this->max_file_size = $max_chunk_size;
 
         $this->current_fragment = 0;
+        $this->wrapped_fragments = array();
         while ($this->filesystem->has($this->current_filename())) {
+            array_push($this->wrapped_fragments, $this->current_filename());
             $this->current_fragment++;
         }
+        if ($this->current_fragment > 0) {
+            $this->current_fragment--; // This will make it append to the last file if it isn't too big yet
+            array_pop($this->wrapped_fragments);
+        }
+    }
+
+    public function get_wrapped_fragments() {
+        return $this->wrapped_fragments;
     }
 
     // TODO this is code duplication with ParkingDeserializer.php
@@ -60,6 +71,7 @@ class FragmentedWriter
     /**
      * Appends content to the fragmented file. Creates new chunk when file size limit is reached.
      * @param $content: The content to append to the file.
+     * @return true if a fragment was wrapped up
      */
     public function write($content) {
         $current_content = "";
@@ -67,18 +79,56 @@ class FragmentedWriter
             $current_content = $this->filesystem->read($this->current_filename());
         }
 
+        $wrapped_fragment = false;
         if (strlen($content) > $this->max_file_size) {
             // If the content itself is bigger than the fragment size, write it but throw a warning
             trigger_error("Content is bigger than the configured chunk size");
+            array_push($this->wrapped_fragments, $this->current_filename());
             $this->current_fragment++;
+            $wrapped_fragment = true;
         } else if (strlen($current_content) + strlen($content) > $this->max_file_size) {
             // If it's not, but it doesn't fit in the fragment, create a new file
+            array_push($this->wrapped_fragments, $this->current_filename());
             $this->current_fragment++;
+            $wrapped_fragment = true;
         } else {
             // If it still fits, append it
             $content = $current_content . $content;
         }
 
         $this->filesystem->put($this->current_filename(), $content . "\n");
+        return $wrapped_fragment;
+    }
+
+    public static function deploy($read_dir, $publish_dir, $fragment_filename) {
+        // Init filesystem
+        $read_adapter = new Local($read_dir);
+        $read_filesystem = new Filesystem($read_adapter);
+        $lines = explode(PHP_EOL, $read_filesystem->read($fragment_filename));
+
+        // Wrap all lines
+        $wrapped_data = array();
+        foreach($lines as $line) {
+            if (strlen($line) > 0) {
+                $data_complete = json_decode($line, true);
+                $data_parkings = array();
+                foreach($data_complete as $entry => $value) {
+                    if (substr($entry, 0, strlen("parking_")) === "parking_") {
+                        $data_parkings[$entry] = $value;
+                    }
+                }
+                $wrapped_data[$data_complete["time"]] = $data_parkings;
+            }
+        }
+
+        // JSON encode to public directory
+        $publish_adapter = new Local($publish_dir);
+        $publish_filesystem = new Filesystem($publish_adapter);
+        if ($publish_filesystem->has("current.json")) {
+            $new_filename = time() . ".json";
+            $publish_filesystem->rename("current.json", $new_filename);
+            $wrapped_data["previous"] = "public/parking/" . $new_filename;
+        }
+        $publish_filesystem->write("current.json", json_encode($wrapped_data));
     }
 }
